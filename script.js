d40c23d40252e9298353d71b51204bbe5c587f96
Daniel <daniel.coppi.dc@gmail.com>
2024-02-11 19:27:24 +0100

    changing version

--- script.js
+++ script.js
@@ -1,47 +1,20 @@
 // SETTING VARABLE
 let root = document.querySelector(":root");
+let screen_w = screen.width;
+let screen_h = screen.height;
 
-// FUNCTION TO CREATE #N STARS
+// FUNCTION TO CREATE STARS
 function multiple_star(n) {
-  let box_shadow = `${Math.floor(Math.random() * 3000)}px ${Math.floor(
-    Math.random() * 3000
-  )}px var(--stars-color)`;
+  let box_shadow = `${Math.floor(
+    Math.random() * (screen_w * 1.5)
+  )}px ${Math.floor(Math.random() * (screen_h * 1.5))}px var(--stars-color)`;
 
   for (let i = 2; i < n; i++) {
     box_shadow = `${box_shadow}, ${Math.floor(
-      Math.random() * 3000
-    )}px ${Math.floor(Math.random() * 3000)}px var(--stars-color)`;
+      Math.random() * (screen_w * 1.5)
+    )}px ${Math.floor(Math.random() * (screen_h * 1.5))}px var(--stars-color)`;
   }
   return box_shadow;
-}
-
-// DARKMODE/LIGHT FUNCTION
-function darkMode() {
-  root.style.setProperty(
-    "--night-gradient",
-    "radial-gradient(ellipse at bottom, #1B2735 0%, #090A0F 100%)"
-  );
-  root.style.setProperty("--stars-color", "white");
-  document
-    .getElementById("light")
-    .style.setProperty("background-color", "transparent");
-  document
-    .getElementById("dark")
-    .style.setProperty("background-color", "var(--stars-color)");
-}
-
-function lightMode() {
-  root.style.setProperty(
-    "--night-gradient",
-    "radial-gradient(ellipse at bottom, #ececec 0%, #c4c4c4 100%)"
-  );
-  root.style.setProperty("--stars-color", "black");
-  document
-    .getElementById("dark")
-    .style.setProperty("background-color", "transparent");
-  document
-    .getElementById("light")
-    .style.setProperty("background-color", "var(--stars-color)");
 }
 
 // PARALLAX FUCNTION
@@ -54,13 +27,113 @@ function parallax(event) {
   });
 }
 
+// LANGUAGE FUNCTIONS
+function updateContent(langData) {
+  document.querySelectorAll("[data-i18n]").forEach((element) => {
+    const key = element.getAttribute("data-i18n");
+    element.textContent = langData[key];
+  });
+} // update content
+
+function setLanguagePreference(lang) {
+  localStorage.setItem("language", lang);
+} // set user's preferred language
+
+async function fetchLanguageData(lang) {
+  const response = await fetch(`languages/${lang}.json`);
+  return response.json();
+} // fetch language data
+
+async function changeLanguage(lang) {
+  await setLanguagePreference(lang);
+
+  const langData = await fetchLanguageData(lang);
+  updateContent(langData);
+} // change language
+
+// THEME FUNCTIONS
+function setThemePreference(theme) {
+  localStorage.setItem("theme", theme);
+}
+
+async function changeMode(theme) {
+  await setThemePreference(theme);
+
+  switch (theme) {
+    case "light":
+      root.style.setProperty(
+        "--night-gradient",
+        "radial-gradient(ellipse at bottom, #ececec 0%, #c4c4c4 100%)"
+      );
+      root.style.setProperty("--stars-color", "black");
+      document.getElementById("dark_box").innerHTML = "□";
+      document.getElementById("light_box").innerHTML = "■";
+      break;
+
+    case "dark":
+      root.style.setProperty(
+        "--night-gradient",
+        "radial-gradient(ellipse at bottom, #1B2735 0%, #090A0F 100%)"
+      );
+      root.style.setProperty("--stars-color", "white");
+      document.getElementById("dark_box").innerHTML = "■";
+      document.getElementById("light_box").innerHTML = "□";
+      break;
+
+    default:
+      root.style.setProperty(
+        "--night-gradient",
+        "radial-gradient(ellipse at bottom, #ececec 0%, #c4c4c4 100%)"
+      );
+      root.style.setProperty("--stars-color", "black");
+      document.getElementById("dark_box").innerHTML = "□";
+      document.getElementById("light_box").innerHTML = "■";
+  }
+}
+
+// NAV FUNCTIONS
+function about() {
+  document.getElementById("li_about").classList.add("is-selected");
+  document.getElementById("li_projects").classList.remove("is-selected");
+  document.getElementById("li_contact").classList.remove("is-selected");
+  document.getElementById("about_me").classList.add("is-selected");
+  document.getElementById("projects").classList.remove("is-selected");
+  document.getElementById("contact").classList.remove("is-selected");
+}
+
+function projects() {
+  document.getElementById("li_about").classList.remove("is-selected");
+  document.getElementById("li_projects").classList.add("is-selected");
+  document.getElementById("li_contact").classList.remove("is-selected");
+  document.getElementById("about_me").classList.remove("is-selected");
+  document.getElementById("projects").classList.add("is-selected");
+  document.getElementById("contact").classList.remove("is-selected");
+}
+
+function contact() {
+  document.getElementById("li_about").classList.remove("is-selected");
+  document.getElementById("li_projects").classList.remove("is-selected");
+  document.getElementById("li_contact").classList.add("is-selected");
+  document.getElementById("about_me").classList.remove("is-selected");
+  document.getElementById("projects").classList.remove("is-selected");
+  document.getElementById("contact").classList.add("is-selected");
+}
+
 // CALLING FUNCTIONS AND  CHANGING CSS
-const creation_stars_small = multiple_star(700);
-const creation_stars_medium = multiple_star(200);
-const creation_stars_big = multiple_star(100);
+const creation_stars_small = multiple_star(800);
+const creation_stars_medium = multiple_star(400);
+const creation_stars_big = multiple_star(150);
 root.style.setProperty("--shadows-small", creation_stars_small);
 root.style.setProperty("--shadows-medium", creation_stars_medium);
 root.style.setProperty("--shadows-big", creation_stars_big);
 
 document.addEventListener("mousemove", parallax);
-document.addEventListener("scroll", parallax);
+document.addEventListener("drag", parallax);
+
+window.addEventListener("DOMContentLoaded", async () => {
+  const userPreferredLanguage = localStorage.getItem("language") || "en";
+  const langData = await fetchLanguageData(userPreferredLanguage);
+  const userPreferredTheme = localStorage.getItem("theme");
+  changeMode(userPreferredTheme);
+  updateContent(langData);
+}); // call updateContent() on page load
