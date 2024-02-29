5fa39bece99e03fef402010323d5609853083be6
Daniel Coppi <daniel.coppi.dc@gmail.com>
2024-02-29 11:28:42 +0100

    permission gyroscope iphone

--- script.js
+++ script.js
@@ -55,7 +55,17 @@ deviceMovement = function () {
     });
   }
 
-  window.addEventListener("deviceorientation", handleOrientation);
+  if (typeof DeviceMotionEvent.requestPermission === "function") {
+    DeviceOrientationEvent.requestPermission()
+      .then((response) => {
+        if (response == "granted") {
+          window.addEventListener("deviceorientation", handleOrientation);
+        }
+      })
+      .catch(console.error);
+  } else {
+    window.addEventListener("deviceorientation", handleOrientation);
+  }
 }; // parallax on smartphone orientation
 
 // LANGUAGE FUNCTIONS
@@ -174,17 +184,7 @@ root.style.setProperty("--shadows-big", creation_stars_big);
 if (viewportWidth >= "1025") {
   document.addEventListener("mousemove", parallax);
 } else if (viewportWidth <= "1024") {
-  if (typeof DeviceMotionEvent.requestPermission === "function") {
-    DeviceOrientationEvent.requestPermission()
-      .then((response) => {
-        if (response == "granted") {
-          deviceMovement();
-        }
-      })
-      .catch(console.error);
-  } else {
-    deviceMovement();
-  }
+  deviceMovement();
 }
 
 window.addEventListener("DOMContentLoaded", async () => {
